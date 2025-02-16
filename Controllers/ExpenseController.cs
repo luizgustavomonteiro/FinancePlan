b6a26d905111638d28b64d52d96f72fe003918f0
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-02-16 14:07:12 -0300

    "Add Models and Controllers"

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinancePlan.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinancePlan.Controllers
@@ -7,5 +8,23 @@ namespace FinancePlan.Controllers
     [ApiController]
     public class ExpenseController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<Expense>>> GetAllExpenses()
+        {
+            var Expenses = new List<Expense>
+            {
+                new Expense {
+                    Id = 1,
+                    Amount = 2,
+                    DateTime = DateTime.Now,
+                    Description = "College Fees",
+                    // Fix how make a category
+                  
+
+                }
+            };
+
+            return Ok(Expenses);
+        }
     }
 }
