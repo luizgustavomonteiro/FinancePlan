213d480d3452170c313faaeb2ac3f1e036d7dc35
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-03-09 15:29:36 -0300

    Added Migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FinancePlan.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinancePlan.Data
 {
     public class DataContext : DbContext 
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
 
     }
 }
